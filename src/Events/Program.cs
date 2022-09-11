83951c5ab5e0c1023fc46e0987c3200321a301ad
COCPORN <cocporn@gmail.com>
2022-09-11 12:52:34 +0200

    Initial commit

---
+++ src/Events/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Logging;
+using OCore.Events;
+using OCore.Services;
+using Orleans;
+
+await OCore.Setup.Developer.LetsGo("Events");
+
+[Event("Test")]
+[GenerateSerializer]
+public class TestEvent
+{
+    [Id(0)]
+    public string? Message { get; init; }
+}
+
+[Service("FireEvent")]
+public interface IFireEventService : IService
+{
+    Task FireEvent(string message);
+}
+
+public class FireEventService : Service, IFireEventService
+{
+    public Task FireEvent(string message)
+    {
+        GrainFactory.GetEventAggregator().Raise(new TestEvent
+        {
+            Message = message
+        });
+        return Task.CompletedTask;
+    }
+}
+
+[Handler("Test")]
+public class TestEventHandler : Handler<TestEvent>
+{
+    private readonly ILogger _logger;
+
+    public TestEventHandler(ILogger<TestEventHandler> logger)
+    {
+        _logger = logger;
+    }
+    protected override Task HandleEvent(TestEvent @event)
+    {
+        _logger.LogInformation(@event.Message);
+        return base.HandleEvent(@event);
+    }
+}
---
+++ src/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using OCore.Services;
+
+await OCore.Setup.Developer.LetsGo("Hello World");
+
+[Service("HelloWorld")]
+public interface IHelloWorld : IService
+{
+    Task<string> Greet(string somebody);
+}
+
+public class HelloWorldService : Service, IHelloWorld
+{
+    int i = 0;
+    public Task<string> Greet(string somebody)
+    {
+        i++;
+        return Task.FromResult($"Hello, {somebody}! {i}");
+    }
+}
